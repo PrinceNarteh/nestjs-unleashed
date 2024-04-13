a5e4288581bf15ed994967f0351897500858962c
Prince Narteh <prinarteh@gmail.com>
2024-04-13 04:52:57 +0000

    feat: Mapping user entity to database table

--- src/domain/users/entities/user.entity.ts
+++ src/domain/users/entities/user.entity.ts
@@ -1,7 +1,23 @@
+import { RegistryDates } from 'common/embedded/registry-dates.embedded';
+import { PrimaryGeneratedColumn, Entity, Column } from 'typeorm';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column({ unique: true })
   email: string;
+
+  @Column({ unique: true })
   phoneNumber: string;
+
+  @Column()
   password: string;
+
+  @Column(() => RegistryDates, { prefix: false })
+  registaryDates: RegistryDates;
 }
