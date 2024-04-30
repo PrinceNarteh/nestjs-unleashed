6b5ce1a35fc7e7646f617335db22ea762178a1d2
Prince Narteh <prinarteh@gmail.com>
2024-04-30 07:23:29 -0700

    feat: implementing payment logic

--- src/domain/payments/payments.service.ts
+++ src/domain/payments/payments.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Payment } from './entities/payment.entity';
+import { Repository } from 'typeorm';
+import { Order } from 'orders/entities/order.entity';
+import { OrderStatus } from 'orders/enums/order-status.enum';
 
 @Injectable()
-export class PaymentsService {}
+export class PaymentsService {
+  constructor(
+    @InjectRepository(Payment)
+    private readonly paymentsRepository: Repository<Payment>,
+    @InjectRepository(Order)
+    private readonly ordersRepository: Repository<Order>,
+  ) {}
+  async payOrder(id: number): Promise<Order> {
+    const order = await this.ordersRepository.findOne({
+      where: { id },
+      relations: { payment: true },
+    });
+    if (!order) {
+      throw new NotFoundException('Order Not Found');
+    }
+    if (order.payment) {
+      throw new ConflictException('Order already paid');
+    }
+    const payment = this.paymentsRepository.create();
+    order.payment = payment;
+    order.status = OrderStatus.AWAITING_SHIPMENT;
+    return this.ordersRepository.save(order);
+  }
+}
